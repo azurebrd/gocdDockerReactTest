12532a82ae730097f7985110ebc5935e6c5667b4
Juancarlos <azurebrd@mangolassi.caltech.edu>
2019-09-03 13:55:54 -0700

    a change to github

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           An edit.<br/>
+          Added a change to github.<br/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
